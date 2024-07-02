544ca863ed5b595be2eafafe25da14412c68dcec
pankajwords12 <167126303+pankajwords12@users.noreply.github.com>
2024-07-02 16:17:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal(`#header`,{origin: 'left'})
+sr.reveal(`#about`,{origin: 'left'})
+sr.reveal(`#Services`,{origin: 'right'})
+sr.reveal(`#Portfolio`,{origin: 'left'})
+sr.reveal(`#contact`,{origin: 'left'})
+
